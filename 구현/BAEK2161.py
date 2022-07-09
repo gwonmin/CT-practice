27b665ad482e4affbd4ebf19962033057f2ca41e
[4기-1반] 양권민 <didrnjsals@gmail.com>
2022-07-09 14:53:41 +0900

    7.9

---
+++ 구현/BAEK2161.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+N = int(input())
+
+card = deque(i for i in range(1,N+1))
+
+while len(card) != 1:
+    print(card.popleft())
+    card.append(card.popleft())
+
+print(card[0])
